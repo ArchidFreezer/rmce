9f7776bf716b9069f784166c52995da818558bf7
Archid <0archid0@gmail.com>
2026-01-15 13:35:52 +0000

    Add unit test for TableRow class

---
+++ test/TableRow_test.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include <table/TableRow.h>
+
+class Cell {
+public:
+	inline Cell(std::string val) : val_{ val } {}
+	inline std::string getVal() const { return val_; }
+	std::string val_;
+};
+
+namespace {
+	TEST(TableRow, FindCell) {
+
+		TableRow<Cell> tr;
+		Cell c1("A");
+		Cell c2("B");
+		tr.addCell(c1);
+		tr.addCell(c2);
+		EXPECT_STREQ(tr.getCell(0).getVal().c_str(), "A");
+		EXPECT_STREQ(tr.getCell(1).getVal().c_str(), "B");
+	}
+
+	TEST(TableRow, Exception) {
+		TableRow<Cell> tr;
+		Cell c1("A");
+		Cell c2("B");
+		tr.addCell(c1);
+		tr.addCell(c2);
+		try {
+			tr.getCell(2);
+			FAIL();
+		} catch (std::out_of_range err) {
+			SUCCEED();
+		} catch (...) {
+			FAIL();
+		}
+
+	}
+}
