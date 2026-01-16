369e1781925de0946e07746791cd682f70cbc418
Archid <0archid0@gmail.com>
2026-01-16 14:07:24 +0000

    Refactor table column matcher to move matched value to base class
    The value used to deterime the coloumn to use in TableColumnMatcher classes was stored in the contrete classes which did not allow references ot the base class ot set the value to match against. This prevented any real polymorphism as this is a primary function of the class.
    
    In order to allow this the base class was templated with the type of data that is used to match columns allowing both a member variable and function to be declared and defined. These were then removed from the existing concrete class.

--- include/ruledata/table/TableColumnCreatureSizeMatcher.h
+++ include/ruledata/table/TableColumnCreatureSizeMatcher.h
@@ -8,9 +8,17 @@
  * 
  * Has CreatureSizeType::kTiny as the default type
  */
-class TableColumnCreatureSizeMatcher : public TableColumnMatcher {
+class TableColumnCreatureSizeMatcher : public TableColumnMatcher<CreatureSizeType::Type> {
 public:
-	int getColumn() {
+
+	/**
+	 * @brief Constructor to set the header type and initialise member variables
+	 */
+	TableColumnCreatureSizeMatcher() : TableColumnMatcher<CreatureSizeType::Type>() {
+		match_ = CreatureSizeType::Type::kTiny;
+	}
+
+	int getColumn() const {
 		using CreatureSizeType::Type;
 		switch (match_) {
 		case Type::kTiny: return 0;
@@ -21,26 +29,4 @@ public:
 		}
 		return 0;
 	}
-
-	/**
-	 * @brief Sets the value to identify the column index
-	 * 
-	 * This function returns a reference to the matcher allowing for method chaining, an example of usage would be:
-	 * 
-	 * @code
-	 * // Use a named matcher
-	 * TableColumnCreatureSizeMatcher matcher = TableColumnCreatureSizeMatcher();
-	 * int col = matcher.setMatch(CreatureSizeType::kLarge).getColumn();
-	 * 
-	 * // Use an anonymous matcher
-	 * int col = TableColumnCreatureSizeMatcher().setMatch(CreatureSizeType::kMedium).getColumn();
-	 * @endcode
-	 * 
-	 * @param match Value to match
-	 * @return TableColumnCreatureSizeMatcher& reference to this matcher
-	 */
-	TableColumnCreatureSizeMatcher& setMatch(CreatureSizeType::Type match) { match_ = match; return *this;  }
-
-private:
-	CreatureSizeType::Type match_{ CreatureSizeType::kTiny }; /**< Stores the type to get the column for */
 };
--- include/ruledata/table/TableColumnMatcher.h
+++ include/ruledata/table/TableColumnMatcher.h
@@ -4,6 +4,7 @@
  * @class TableColumnMatcher
  * @brief Abstract class to identify the column index in a table
  */
+template<class ColumnType>
 class TableColumnMatcher {
 public:
 	/**
@@ -15,5 +16,27 @@ public:
 	 * @brief Get the index of the table column
 	 * @return int table column index
 	 */
-	virtual int getColumn() = 0;
+	virtual int getColumn() const = 0;
+
+	/**
+	 * @brief Sets the value to identify the column index
+	 *
+	 * This function returns a reference to the matcher allowing for method chaining, an example of usage would be:
+	 *
+	 * @code
+	 * // Use a named matcher
+	 * TableColumnMatcherDerivative matcher = TableColumnMatcher();
+	 * int col = matcher.setMatch(CreatureSizeType::kLarge).getColumn();
+	 *
+	 * // Use an anonymous matcher
+	 * int col = TableColumnMatcherDerivative().setMatch(CreatureSizeType::kMedium).getColumn();
+	 * @endcode
+	 *
+	 * @param match Value to match
+	 * @return TableColumnMatcher& reference to this matcher
+	 */
+	TableColumnMatcher& setMatch(ColumnType match) { match_ = match; return *this; }
+
+protected:
+	ColumnType match_; /**< Stores the value used to match the column */
 };
