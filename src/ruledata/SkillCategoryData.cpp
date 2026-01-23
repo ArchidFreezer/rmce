f64d9d8d6947e65d79550b62998322fb560bc6f0
Archid <0archid0@gmail.com>
2026-01-23 21:04:53 +0000

    Add missing skill category rank progression
    The skill progression used for skill categories was missing and the categories wee incorrectly defined as using the skill progression. This commit adds the missing progression and assigns it to categories where appropriate.

--- src/ruledata/SkillCategoryData.cpp
+++ src/ruledata/SkillCategoryData.cpp
@@ -23,10 +23,10 @@ bool SkillCategoryData::removeStat(StatType::Type stat) {
 
 void SkillCategoryData::setSkillProgressions(const SkillProgressionTypeData& skill_progression, const SkillProgressionTypeData& skill_category_progression) {
 	// Check for invlaid combinations first
-	if (skill_category_progression.name() != "Standard" && skill_category_progression.name() != "None") {
+	if (skill_category_progression.name() != "Category Standard" && skill_category_progression.name() != "None") {
 		throw InvalidSkillProgression("Category progression may only be Standard or None.");
 	}
-	if (skill_category_progression.name() == "Standard" && skill_progression.name() != "Standard") {
+	if (skill_category_progression.name() == "Category Standard" && skill_progression.name() != "Standard") {
 		throw InvalidSkillProgression("Category progression may only be Standard if the skill progression is also set to Standard.");
 	}
 	// Combination is valid
