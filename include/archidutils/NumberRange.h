ea78b08e018a5791b94899fb2d9e210ed1dc5d5c
Archid <0archid0@gmail.com>
2026-01-25 17:45:42 +0000

    Fix incorrect member return types
    The NumberRange class is templated to allow for any integral or floating point but the member functions that return the min and max values always return an int. This commit fixes this so the return the templated type correctly.

--- include/archidutils/NumberRange.h
+++ include/archidutils/NumberRange.h
@@ -48,13 +48,13 @@ public:
 	 * @brief Get the minimum number that this range matches
 	 * @return int Minimum matched number
 	 */
-	int min() const { return min_; }
+	NumberType min() const { return min_; }
 
 	/**
 	 * @brief Get the maximum number that this range matches
 	 * @return int Maximum matched number
 	 */
-	int max() const { return max_; }
+	NumberType max() const { return max_; }
 
 private:
 	NumberType min_{}; /**< Lower bound of the range */
