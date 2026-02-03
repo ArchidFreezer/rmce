868dbd26819bd5dd6649e0675f34578201341b2a
Archid <0archid0@gmail.com>
2026-02-03 22:42:35 +0000

    Update armour type parsers to use game data factory

--- src/dataparsers/ArmourTypeDatafileParserJson.cpp
+++ src/dataparsers/ArmourTypeDatafileParserJson.cpp
@@ -1,6 +1,7 @@
 #include <ArmourType.h>
 #include <ArmourTypeData.h>
 #include <ArmourTypeDatafileParserJson.h>
+#include <GameRuleDataFactory.h>
 
 void ArmourTypeDatafileParserJson::populateDatum(std::string& id, pt::ptree& datum) {
 	ArmourTypeData& game_data = cache().get<ArmourTypeData>(id);
@@ -22,6 +23,8 @@ void ArmourTypeDatafileParserJson::parse(bool id_only) {
 	// We know there are no references in armour types so we create the complete object in the cache on the first pass
 	if (!id_only) return;
 
+	GameRuleDataFactory factory(cache());
+
 	std::cout << "Loading ArmourType data ... " << std::endl;
 
 	// Get the data objects to parse and loop through them
@@ -32,27 +35,26 @@ void ArmourTypeDatafileParserJson::parse(bool id_only) {
 
 		// We create a ArmourTypeData object and reference it with as a unique_ptr to allow us to use move semantics to transfer
 		// ownership to the cache when we add it
-		std::unique_ptr<ArmourTypeData> datum = std::make_unique<ArmourTypeData>(id);
-		datum->setName(name);
-		datum->setDescription(v.second.get<std::string>("description"));
-		datum->setMinManoeuvreMod(v.second.get<int>("min-manoeuvre-mod"));
-		datum->setMaxManoeuvreMod(v.second.get<int>("max-manoeuvre-mod"));
-		datum->setMissileAttackPenalty(v.second.get<int>("missile-attack-penalty"));
-		datum->setQuicknessPenalty(v.second.get<int>("quickness-penalty"));
-		datum->setAnimalOnly(v.second.get<bool>("animal-only"));
-		datum->setIncludesGreaves(v.second.get<bool>("includes-greaves"));
+		ArmourTypeData& datum = factory.get<ArmourTypeData>(id);
+		datum.setName(name);
+		datum.setDescription(v.second.get<std::string>("description"));
+		datum.setMinManoeuvreMod(v.second.get<int>("min-manoeuvre-mod"));
+		datum.setMaxManoeuvreMod(v.second.get<int>("max-manoeuvre-mod"));
+		datum.setMissileAttackPenalty(v.second.get<int>("missile-attack-penalty"));
+		datum.setQuicknessPenalty(v.second.get<int>("quickness-penalty"));
+		datum.setAnimalOnly(v.second.get<bool>("animal-only"));
+		datum.setIncludesGreaves(v.second.get<bool>("includes-greaves"));
 
 		// Attempt to find the ArmourType
 		std::string type_name = v.second.get<std::string>("type");
 		ArmourType::Type type;
 		if (ArmourType::fromString(type_name)) {
-			datum->setType(ArmourType::fromString(type_name).value());
+			datum.setType(ArmourType::fromString(type_name).value());
 		} else {
 			throw ArmourType::ArmourTypeNotFoundException("Could not find a skill group for: " + type_name);
 		}
 
-		std::cout << "\tArmourType name: " << datum->name() << std::endl;
-		cache().add<ArmourTypeData>(std::move(datum), id);
+		std::cout << "\tArmourType name: " << datum.name() << std::endl;
 
 	}
 	std::cout << " done" << std::endl;
