5e2403d5a6588f1bce1b2c8aec9a1311c09cdf65
Archid <0archid0@gmail.com>
2026-02-07 12:58:02 +0000

    Prevent dice from being in invalid state
    In order to prevent infinite loops the dice object would silently modify some parameter values which can cause unintended side effects. This commit updates the object to throw exceptions instead for invalid argument values

--- src/archidutils/Dice.cpp
+++ src/archidutils/Dice.cpp
@@ -3,7 +3,11 @@
 
 Dice::Dice(int sides) : Dice(sides, 0, 0) {}
 Dice::Dice(int sides, int open_ended_range) : Dice(sides, open_ended_range, open_ended_range) {}
-Dice::Dice(int sides, int open_ended_high_range, int open_ended_low_range) : sides_{ std::max(sides, 1) }, min_open_high_{ std::max(2,sides - open_ended_high_range + 1) }, max_open_low_{ std::min(open_ended_low_range, sides_ - 1) } {}
+Dice::Dice(int sides, int open_ended_high_range, int open_ended_low_range) : sides_{sides}, min_open_high_{ sides - open_ended_high_range + 1 }, max_open_low_{ open_ended_low_range } {
+	if (sides_ < 0) throw std::invalid_argument("Number of sides on the dice must not be negative");
+	if (min_open_high_ <= 1)  throw std::invalid_argument("Open ended high minimum must be greater than 1");
+	if (max_open_low_ >= sides_)  throw std::invalid_argument("Open ended low maximum must be less than the number of sides");
+}
 
 Dice& Dice::roll(bool open_ended) {
 	open_ended ? rollOpenEnded() : roll(1);
@@ -14,13 +18,17 @@ Dice& Dice::roll(int num_dice) {
 	// Don't use silly values
 	assert(num_dice > 0 && "Attempt to roll dice with too few die");
 
-	int num_rolls{ std::max(1,num_dice) };
-	int result{};
+	if (sides_ == 0) {
+		result_ = std::make_pair(0, 0);
+	} else {
+		int num_rolls{ std::max(1,num_dice) };
+		int result{};
 
-	for (int i = 0; i < num_rolls; i++) {
-		result += Random::get(1, sides_);
+		for (int i = 0; i < num_rolls; i++) {
+			result += Random::get(1, sides_);
+		}
+		result_ = std::make_pair(result, result);
 	}
-	result_ = std::make_pair(result, result);
 	return *this;
 }
 
