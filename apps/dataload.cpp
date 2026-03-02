f3f094c465a0a2f3b9886d3479ad837a740e2ad8
Archid <0archid0@gmail.com>
2026-03-02 21:59:44 +0000

    Refactor poison data to use new serializer classes
    Replaced PoisonDatafileParserJson and PoisonTypeDatafileParserJson with PoisonSerializer and PoisonTypeSerializer, which inherit from PersistentObjectJsonSerializer. Updated dataload.cpp and CMakeLists.txt to use the new serializers for loading and saving poison and poison type data. This change modernizes and unifies the data handling approach for poisons, making it consistent with other data types and leveraging persistent object management.

--- apps/dataload.cpp
+++ apps/dataload.cpp
@@ -17,8 +17,8 @@
 #include <PersistentCache.h>
 #include <PersistentObjectFileSerializer.h>
 #include <PersistentObjectManager.h>
-#include <PoisonDatafileParserJson.h>
-#include <PoisonTypeDatafileParserJson.h>
+#include <PoisonSerializer.h>
+#include <PoisonTypeSerializer.h>
 #include <ProfessionDatafileParserXml.h>
 #include <ProfessionDatefileParserJson.h>
 #include <RaceDatafileParserJson.h>
@@ -54,8 +54,8 @@ int main() {
 	PersistentObjectFileSerializer<DiseaseTypeSerializer> disease_type_serializer(object_manager, "../../../../data/DiseaseTypes.json");
 	PersistentObjectFileSerializer<LanguageCategorySerializer> language_category_serializer(object_manager, "../../../../data/LanguageCategories.json");
 	PersistentObjectFileSerializer<LanguageSerializer> language_serializer(object_manager, "../../../../data/Languages.json");
-	PoisonDatafileParserJson poison_parser(object_manager, "../../../../data/Poisons.json");
-	PoisonTypeDatafileParserJson poison_type_parser(object_manager, "../../../../data/PoisonTypes.json");
+	PersistentObjectFileSerializer<PoisonSerializer> poison_serializer(object_manager, "../../../../data/Poisons.json");
+	PersistentObjectFileSerializer<PoisonTypeSerializer> poison_type_serializer(object_manager, "../../../../data/PoisonTypes.json");	
 	ProfessionDatafileParserJson profession_parser(object_manager, "../../../../data/Professions.json");
 	RaceDatafileParserJson race_parser(object_manager, "../../../../data/Races.json");
 	SkillCategoryDatafileParserJson skill_category_parser(object_manager, "../../../../data/SkillCategories.json");
@@ -71,8 +71,6 @@ int main() {
 
 	// Store the parsers in a vector so we can iterate through them
 	std::vector<DatafileParser*> parsers;
-	parsers.push_back(&poison_parser);
-	parsers.push_back(&poison_type_parser);
 	parsers.push_back(&profession_parser);
 	parsers.push_back(&race_parser);
 	parsers.push_back(&skill_category_parser);
@@ -98,6 +96,8 @@ int main() {
 	serializers.push_back(&disease_type_serializer);
 	serializers.push_back(&language_category_serializer);
 	serializers.push_back(&language_serializer);
+	serializers.push_back(&poison_serializer);
+	serializers.push_back(&poison_type_serializer);
 	serializers.push_back(&training_package_cost_table);
 
 
@@ -115,8 +115,6 @@ int main() {
 			serializer->read();
 		}
 
-		// poison_parser.save("../../../../data/Poisons2.json");
-		// poison_type_parser.save("../../../../data/PoisonTypes2.json");
 		// profession_parser.save("../../../../data/Professions2.json");
 		// race_parser.save("../../../../data/Races2.json");
 		// skill_category_parser.save("../../../../data/SkillCategories2.json");
@@ -141,8 +139,8 @@ int main() {
 		disease_type_serializer.save("../../../../data2/DiseaseTypes.json");
 		language_category_serializer.save("../../../../data2/LanguageCategories.json");
 		language_serializer.save("../../../../data2/Languages.json");
-		poison_parser.save("../../../../data2/Poisons.json");
-		poison_type_parser.save("../../../../data2/PoisonTypes.json");
+		poison_serializer.save("../../../../data2/Poisons.json");
+		poison_type_serializer.save("../../../../data2/PoisonTypes.json");
 		profession_parser.save("../../../../data2/Professions.json");
 		race_parser.save("../../../../data2/Races.json");
 		skill_category_parser.save("../../../../data2/SkillCategories.json");
---
+++ include/rm/rule/parser/PoisonSerializer.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <PersistentObjectJsonSerializer.h>
+#include <PoisonData.h>
+
+namespace rm::rule::parser {
+
+/**
+ * @class PoisonSerializer
+ * @brief Process PoisonData objects for json files
+ *
+ * This class can read a well-formed stream creating JSON formatted objects parsing the contents and adding them to a cache.
+ *
+ * The reverse operation is also available where the objects from the cache may be written out to a stream in well-formed JSON format.
+ *
+ * @see PoisonData
+ */
+
+class PoisonSerializer : public PersistentObjectJsonSerializer {
+public:
+	/**
+	 * @brief Type alias for the data class handled by this serializer
+	 */
+	using DataClass = PoisonData;
+
+	/**
+	 * @brief Constructor
+	 * @param object_manager Reference to an object manager to handle the data objects
+	 */
+	PoisonSerializer(rm::PersistentObjectManager& object_manager) : PersistentObjectJsonSerializer(object_manager, "Poison") {
+	}
+
+private:
+	/**
+	 * @brief Parse a ptree into PoisonData objects
+	 *
+	 * Parse a boost::ptree containing the poison rule data, convert to PoisonData objects and store in a data cache
+	 */
+	void parse(pt::ptree& ptree) override;
+
+	/**
+	 * @brief Populate the given boost tree with the data from a poison
+	 * @param key Id of the poison to populate from
+	 * @param pdatum boost tree to populate
+	 */
+	void populateDatum(std::string key, pt::ptree& datum) override;
+
+	/**
+	 * @brief Getter for the root node name to use when writing the data to a json file
+	 * @return String containing the root node name to use when writing the data to a json file
+	 */
+	std::string rootNode() const;
+};
+
+} // namespace rm::rule::parser
