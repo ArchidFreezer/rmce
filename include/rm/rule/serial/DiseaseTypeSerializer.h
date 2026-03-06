184829c61e6a03c2eb7f1763a60e61f832b1efb3
Archid <0archid0@gmail.com>
2026-03-06 11:15:45 +0000

    Add DiseaseTypeSerializer for JSON (de)serialization
    Introduced DiseaseTypeSerializer.cpp/h to handle JSON serialization and deserialization of DiseaseTypeData objects. Updated CMakeLists.txt to include the new files. The serializer supports fields like id, type, transmission, description, and severity-symptoms, using JsonConverter utilities and enum string mapping.

---
+++ include/rm/rule/serial/DiseaseTypeSerializer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <PersistentObjectSerializer.h>
+#include <DiseaseTypeData.h>
+
+namespace rm::rule::serial {
+/**
+ * @class DiseaseTypeSerializer
+ * @brief JSON serializer for DiseaseTypeData objects
+ */
+class DiseaseTypeSerializer : public PersistentObjectSerializer<DiseaseTypeData> {
+public:
+	/* We need to bring the base class serializeObject method into scope to allow us to use the overload that takes an ID as well as the one that takes a DiseaseTypeData object */
+	using PersistentObjectSerializer<DiseaseTypeData>::serializeObject;
+
+	/**
+	 * @brief Constructor for the DiseaseTypeSerializer class.
+	 * @param manager Object manager to retrive persistent objects from or add to
+	 */
+	DiseaseTypeSerializer(rm::PersistentObjectManager& manager) : PersistentObjectSerializer<DiseaseTypeData>(manager) {
+	}
+
+	/**
+	 * @brief Serialize a DiseaseTypeData object to a JSON value
+	 *
+	 * @param ref Reference to the DiseaseTypeData object to serialize
+	 * @return json::value containing the serialized DiseaseTypeData object
+	 */
+	json::value serializeObject(const DiseaseTypeData& ref) const override;
+
+	/**
+	 * @brief Deserialize a DiseaseTypeData object from a JSON value
+	 *
+	 * @param jsonObj JSON object containing the data to deserialize
+	 * @return Reference to the deserialized DiseaseTypeData object
+	 */
+	const DiseaseTypeData& deserializeObject(json::object& jsonObj) const override;
+};
+} // namespace rm::rule::serial
