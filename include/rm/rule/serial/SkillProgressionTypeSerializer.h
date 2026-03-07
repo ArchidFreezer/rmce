684539b46fad422c91079f7862a792e2f864c6d8
Archid <0archid0@gmail.com>
2026-03-07 09:58:17 +0000

    Add SkillProgressionType serializer and update JSON format
    Created SkillProgressionTypeSerializer for JSON (de)serialization.
    Added it to CMakeLists.txt for build inclusion.
    Updated SkillProgressionTypes.json to use float values in scientific notation instead of strings for numeric fields.

---
+++ include/rm/rule/serial/SkillProgressionTypeSerializer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <PersistentObjectSerializer.h>
+#include <SkillProgressionTypeData.h>
+
+namespace rm::rule::serial {
+/**
+ * @class SkillProgressionTypeSerializer
+ * @brief JSON serializer for SkillProgressionTypeData objects
+ */
+class SkillProgressionTypeSerializer : public PersistentObjectSerializer<SkillProgressionTypeData> {
+public:
+	/* We need to bring the base class serializeObject method into scope to allow us to use the overload that takes an ID as well as the one that takes a SkillProgressionTypeData object */
+	using PersistentObjectSerializer<SkillProgressionTypeData>::serializeObject;
+
+	/**
+	 * @brief Constructor for the SkillProgressionTypeSerializer class.
+	 * @param manager Object manager to retrive persistent objects from or add to
+	 */
+	SkillProgressionTypeSerializer(rm::PersistentObjectManager& manager) : PersistentObjectSerializer<SkillProgressionTypeData>(manager) {
+	}
+
+	/**
+	 * @brief Serialize a SkillProgressionTypeData object to a JSON value
+	 *
+	 * @param ref Reference to the SkillProgressionTypeData object to serialize
+	 * @return json::value containing the serialized SkillProgressionTypeData object
+	 */
+	json::value serializeObject(const SkillProgressionTypeData& ref) const override;
+
+	/**
+	 * @brief Deserialize a SkillProgressionTypeData object from a JSON value
+	 *
+	 * @param jsonObj JSON object containing the data to deserialize
+	 * @return Reference to the deserialized SkillProgressionTypeData object
+	 */
+	const SkillProgressionTypeData& deserializeObject(json::object& jsonObj) const override;
+};
+} // namespace rm::rule::serial
