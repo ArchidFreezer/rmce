f572a90e31c64b2f1adc0d7eca2f7a5b605fb9bf
Archid <0archid0@gmail.com>
2026-03-07 12:18:17 +0000

    Add SpellListSerializer and reformat SpellLists.json
    Introduced SpellListSerializer for JSON serialization/deserialization of SpellListData. Added new header and implementation files, updated build configuration, and reformatted SpellLists.json for consistency and improved readability.

---
+++ include/rm/rule/serial/SpellListSerializer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <PersistentObjectSerializer.h>
+#include <SpellListData.h>
+
+namespace rm::rule::serial {
+/**
+ * @class SpellListSerializer
+ * @brief JSON serializer for SpellListData objects
+ */
+class SpellListSerializer : public PersistentObjectSerializer<SpellListData> {
+public:
+	/* We need to bring the base class serializeObject method into scope to allow us to use the overload that takes an ID as well as the one that takes a SpellListData object */
+	using PersistentObjectSerializer<SpellListData>::serializeObject;
+
+	/**
+	 * @brief Constructor for the SpellListSerializer class.
+	 * @param manager Object manager to retrive persistent objects from or add to
+	 */
+	SpellListSerializer(rm::PersistentObjectManager& manager) : PersistentObjectSerializer<SpellListData>(manager) {
+	}
+
+	/**
+	 * @brief Serialize a SpellListData object to a JSON value
+	 *
+	 * @param ref Reference to the SpellListData object to serialize
+	 * @return json::value containing the serialized SpellListData object
+	 */
+	json::value serializeObject(const SpellListData& ref) const override;
+
+	/**
+	 * @brief Deserialize a SpellListData object from a JSON value
+	 *
+	 * @param jsonObj JSON object containing the data to deserialize
+	 * @return Reference to the deserialized SpellListData object
+	 */
+	const SpellListData& deserializeObject(json::object& jsonObj) const override;
+};
+} // namespace rm::rule::serial
