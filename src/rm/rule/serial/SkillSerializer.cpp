d9ecfc14c7f8705a9c074a2b81a8f72ad4ccf797
Archid <0archid0@gmail.com>
2026-03-07 11:22:16 +0000

    Add SkillSerializer and refactor Skills.json structure
    Refactored Skills.json to standardize skill entry formatting, converting "exhaustion" and "distance-multiplier" to numeric values and integrating "stats" directly within each skill object. Introduced SkillSerializer class for JSON serialization/deserialization of SkillData, with build integration and full support for persistent object management. These changes improve data clarity, maintainability, and enable seamless SkillData persistence.

---
+++ src/rm/rule/serial/SkillSerializer.cpp
@@ -0,0 +1,54 @@
+#include <SkillSerializer.h>
+#include <JsonConverter.h>
+
+namespace rm::rule::serial {
+
+json::value SkillSerializer::serializeObject(const SkillData& ref) const {
+	json::object obj;
+
+	JsonConverter::setString(obj, "id", ref.id());
+	JsonConverter::setString(obj, "name", ref.name());
+	JsonConverter::setString(obj, "category", ref.category().id());
+	JsonConverter::setString(obj, "description", ref.description());
+	JsonConverter::setString(obj, "book", ref.book().id());
+	JsonConverter::setString(obj, "action", toString(ref.actionType()));
+	JsonConverter::setString(obj, "difficulties-summary", ref.difficultySummary());
+	JsonConverter::setString(obj, "notes", ref.notes());
+	JsonConverter::setBool(obj, "is-restricted", ref.restricted());
+	JsonConverter::setBool(obj, "can-specialise", ref.canSpecialise());
+	JsonConverter::setBool(obj, "mandatory-subcategory", ref.mandatorySubcategory());
+	JsonConverter::setStringArray(obj, "subcategories", ref.subcategories());
+	JsonConverter::setEnumSet(obj, "stats", ref.stats());
+	JsonConverter::setFloat(obj, "exhaustion", ref.exhaustionCost());
+	JsonConverter::setFloat(obj, "distance-multiplier", ref.distanceMultiplier());
+
+	return obj;
+}
+
+const SkillData& SkillSerializer::deserializeObject(json::object& jsonObj) const {
+	std::string id = JsonConverter::getString(jsonObj, "id");
+	SkillData& ref = manager_.get<SkillData>(id);
+	ref.setName(JsonConverter::getString(jsonObj, "name"));
+	ref.setCategory(manager_.get<SkillCategoryData>(JsonConverter::getString(jsonObj, "category")));
+	ref.setDescription(JsonConverter::getString(jsonObj, "description"));
+	ref.setBook(manager_.get<BookData>(JsonConverter::getString(jsonObj, "book")));
+	
+	// Set action type
+	SkillActionType::Type action_type{};
+	fromString(JsonConverter::getString(jsonObj, "action"), action_type);
+	ref.setActionType(action_type);
+
+	ref.setDifficultySummary(JsonConverter::getString(jsonObj, "difficulties-summary"));
+	ref.setNotes(JsonConverter::getString(jsonObj, "notes"));
+	ref.setIsRestricted(JsonConverter::getBool(jsonObj, "is-restricted", false));
+	ref.setCanSpecialise(JsonConverter::getBool(jsonObj, "can-specialise", false));
+	ref.setMandatorySubcategory(JsonConverter::getBool(jsonObj, "mandatory-subcategory", false));
+	ref.setSubcategories(JsonConverter::getStringSet(jsonObj, "subcategories"));
+	ref.setStats(JsonConverter::getEnumVector<StatType::Type>(jsonObj, "stats"));
+	ref.setExhaustionCost(JsonConverter::getFloat(jsonObj, "exhaustion", 0.0f));
+	ref.setDistanceMultiplier(JsonConverter::getFloat(jsonObj, "distance-multiplier", 1.0f));
+
+	return ref;
+}
+
+} // namespace rm::rule::serial
