60ad3a053e9d5ed922717101a49da1d5c35e80de
Archid <0archid0@gmail.com>
2026-03-07 14:38:06 +0000

    Refactor serializer logic into generic base class
    Introduce PersistentSerializer as a generic base class for persistent object serialization/deserialization. Refactor PersistentJsonSerializer to inherit from it, removing redundant code and centralizing manager handling and copy/move operator deletion. This enables flexible serialization formats and improves code reuse and maintainability.

--- include/rm/rule/serial/PersistentJsonSerializer.h
+++ include/rm/rule/serial/PersistentJsonSerializer.h
@@ -1,7 +1,6 @@
 #pragma once
-#include <string>
 #include <boost/json.hpp>
-#include <PersistentObjectManager.h>
+#include <PersistentSerializer.h>
 
 namespace rm::rule::serial {
 
@@ -14,14 +13,14 @@ namespace json = boost::json;
  * The copy and move constructors and assignment operators are deleted to prevent slicing and ensure that the class is only used as a base class for specific serializers.
  */
 template<persistent_object PersistentObject>
-class PersistentJsonSerializer {
+class PersistentJsonSerializer : public PersistentSerializer<PersistentObject, json:: value, json::object> {
 public:
 	/**
 	 * @brief Constructor for the PersistentJsonSerializer class.
 	 *
 	 * @param manager Reference to the PersistentObjectManager used for managing persistent objects during serialization and deserialization.
 	 */
-	PersistentJsonSerializer(rm::PersistentObjectManager& manager) : manager_(manager) {
+	PersistentJsonSerializer(rm::PersistentObjectManager& manager) : PersistentSerializer<PersistentObject, json::value, json::object>(manager) {
 	}
 
 	/**
@@ -29,51 +28,6 @@ public:
 	 */
 	virtual ~PersistentJsonSerializer() = default;
 
-	/*
-	 * We delete all the copy/move constructors and assignment operators to prevent slicing and ensure that the class is only used as a base class for specific serializers.
-	 */
-	PersistentJsonSerializer(const PersistentJsonSerializer&) = delete;            /**< Delete the copy constructor */
-	PersistentJsonSerializer& operator=(const PersistentJsonSerializer&) = delete; /**< Delete the copy assignment operator */
-	PersistentJsonSerializer(PersistentJsonSerializer&&) = delete;                 /**< Delete the move constructor */
-	PersistentJsonSerializer& operator=(PersistentJsonSerializer&&) = delete;      /**< Delete the move assignment operator */
-
-	/**
-	 * @brief Serialize a single object to a JSON value
-	 *
-	 * @param obj Reference to the object to serialize
-	 * @return json::value containing the serialized object
-	 */
-	virtual json::value serializeObject(const PersistentObject& obj) const = 0;
-
-	/**
-	 * @brief Serialize a single object to a JSON value by its ID
-	 *
-	 * @param id Unique ID of the object to serialize
-	 * @return json::value containing the serialized object
-	 */
-	json::value serializeObject(const std::string& id) const {
-		return serializeObject(manager_.get<PersistentObject>(id));
-	}
-
-	/**
-	 * @brief Deserialize a single object from a JSON value
-	 *
-	 * @param jsonObj JSON object containing the data to deserialize
-	 * @return Reference to the deserialized object
-	 */
-	virtual const PersistentObject& deserializeObject(json::object& jsonObj) const = 0;
-
-	/**
-	 * @brief Get the PersistentObjectManager used by this serializer
-	 *
-	 * @return Reference to the PersistentObjectManager
-	 */
-	rm::PersistentObjectManager& manager() const {
-		return manager_;
-	}
-
-protected:
-	rm::PersistentObjectManager& manager_; /**< Object manager to retrive persistent objects from or add to */
 };
 
 } // namespace rm::rule::serial
