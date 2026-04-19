ac18cd09ce15c3ee8a6288b43336bc639954f9ac
ArchidFreezer <98521914+ArchidFreezer@users.noreply.github.com>
2026-04-19 22:35:43 +0100

    Dummy (#92)
    Remove unused blank lines
    
    ---------
    
    Co-authored-by: Archid <0archid0@gmail.com>

--- src/rm/game/Character.cpp
+++ src/rm/game/Character.cpp
@@ -46,10 +46,9 @@ void Character::updateStatDerivedData(StatType::Type stat_type) {
 	default:
 		break;
 	}
-
 }
 
-	int Character::skillBonus(const SubcategoriedSkillData& skill) const {
+int Character::skillBonus(const SubcategoriedSkillData& skill) const {
 	int bonus{0};
 
 	auto it = skills_.find(&skill);
