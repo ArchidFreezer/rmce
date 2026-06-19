9b3ca68f0d76aca01d281b0134f190809f3bcfdc
Archid <0archid0@gmail.com>
2026-06-19 17:42:52 +0100

    Add foraging food and water manoeuvre actions
    Added methods to forage for food and water in a location. Renamed the modifier struct for herb foraging to distinguish between the different foraging actions.

--- include/rm/action/Forage.h
+++ include/rm/action/Forage.h
@@ -10,17 +10,91 @@
 
 namespace rm::action::forage {
 
-struct Config {
+/**
+ * @brief Struct to hold modifiers for the herb foraging action.
+ *
+ * This struct contains various parameters that can affect the outcome of the herb foraging action, such as the target resource, hours spent foraging, number of additional searchers, and any skill bonuses.
+ */
+struct HerbModifiers {
 	rm::PersistentObjectManager& object_factory;              /**< The persistent object manager */
 	const rm::rule::ForagableData* target_resource = nullptr; /**< Optional target resource to forage for, if not provided, all resources will be considered */
 	int hours_spent{10};                                      /**< Number of hours spent foraging, used to determine the skill check difficulty and the number of doses found */
 	int additional_searchers{0};                              /**< Number of additional searchers */
 	int failed_search_count{0};                               /**< Number of unsuccessful search attempts */
+	bool hunted{false};                                       /**< Whether there is a hue and cry out on the forager */
 
 	int herb_lore_bonus{0};                                       /**< Generic herb lore skill bonus from items such as a book */
 	int poison_lore_bonus{0};                                     /**< Generic poison lore skill bonus from items such as a book */
 	std::map<const rm::rule::ForagableData*, int> lore_bonuses{}; /**< Map of foragable resources and any herb lore skill bonuses from items (book) or familiarity with specific plants */
 };
 
-std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::Character& forager, rm::game::Location& search_location, Config& config);
+/**
+ * @brief Forage for herbs in a given location using the provided character and modifiers.
+ *
+ * @param forager The character who is foraging.
+ * @param search_location The location where the foraging is taking place.
+ * @param modifiers The modifiers that affect the foraging process.
+ *
+ * @return A map of found and identified foragable resources and the number of doses found for each resource.
+ */
+std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::Character& forager, rm::game::Location& search_location, HerbModifiers& modifiers);
+
+/**
+ * @brief Struct to hold modifiers for the sustainance foraging action.
+ *
+ * This struct contains various parameters that can affect the outcome of the sustainance foraging action, such as the number of hours spent foraging, number of people sustainance is required for, and whether it is the dry season.
+ */
+struct SustainanceModifiers {
+	rm::PersistentObjectManager& object_factory; /**< The persistent object manager */
+	int hours_spent{4};                          /**< Number of hours spent foraging, used to determine the skill check difficulty */
+	int num_people{1};                           /**< Number of people sustainance is required for */
+	bool dry_season{false};                      /**< Whether it is the dry season, which makes foraging more difficult */
+};
+
+/**
+ * @brief Forage for sustainance in a given location using the provided character and modifiers.
+ *
+ * Foraging for sustainance is for finding water and plants for survival, it includes fishing  and clamming, but does not include hunting for meat. The foraging process is affected by the number of hours spent foraging, the number of people
+ * sustainance is required for, and whether it is the dry season.
+ *
+ * The foraging of food and water are treated as separate skill checks as the difficulty of finding food and water can be different. A single roll is used for both resources but the skill check difficulty is calculated separately for each
+ * resource. The amount of food and water found is calculated based on the skill check roll and the number of hours spent foraging.
+ *
+ * The default duration of the action is one strategic round (4 hrs) and each additional 2 hours spent foraging decreasing the difficulty by 1. The base roll is to provide enough food and water for 1 individual, with each additional person
+ * increasing the difficulty by 1. The dry season makes foraging for water more difficult in locations where there are monsoons/dry seasons.
+ *
+ * @param forager The character who is foraging.
+ * @param search_location The location where the foraging is taking place.
+ * @param modifiers The modifiers that affect the foraging process.
+ *
+ * @return A pair of floats representing the amount of food and water found relative to the required amount.
+ */
+std::pair<float, float> forage_sustainance(rm::game::character::Character& forager, rm::game::Location& search_location, SustainanceModifiers& modifiers);
+
+/**
+ * @brief Forage for food in a given location using the provided character and modifiers.
+ *
+ * This function is a wrapper around forage_sustainance that only returns the amount of food found. It is provided for convenience when only the food foraging result is needed.
+ *
+ * @param forager The character who is foraging.
+ * @param search_location The location where the foraging is taking place.
+ * @param modifiers The modifiers that affect the foraging process.
+ *
+ * @return A float representing the amount of food found relative to the required amount.
+ */
+float forage_food(rm::game::character::Character& forager, rm::game::Location& search_location, SustainanceModifiers& modifiers);
+
+/**
+ * @brief Forage for water in a given location using the provided character and modifiers.
+ *
+ * This function is a wrapper around forage_sustainance that only returns the amount of water found. It is provided for convenience when only the water foraging result is needed.
+ *
+ * @param forager The character who is foraging.
+ * @param search_location The location where the foraging is taking place.
+ * @param modifiers The modifiers that affect the foraging process.
+ *
+ * @return A float representing the amount of water found relative to the required amount.
+ */
+float forage_water(rm::game::character::Character& forager, rm::game::Location& search_location, SustainanceModifiers& modifiers);
+
 } // namespace rm::action::forage
--- src/rm/action/Forage.cpp
+++ src/rm/action/Forage.cpp
@@ -6,7 +6,7 @@
 
 namespace rm::action::forage {
 
-std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::Character& forager, rm::game::Location& search_location, Config& config) {
+std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::Character& forager, rm::game::Location& search_location, HerbModifiers& modifiers) {
 	archid::Dice d100(100, 5); // Create a d100 dice with open-ended rolls for the top and bottom 5 results
 
 	std::map<const rm::rule::ForagableData*, int> foraged_resources;
@@ -23,31 +23,32 @@ std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::
 	 * Perform foraging skill check to see what resources are found in the search location. The skill check is modified by the number of hours spent foraging, the number of additional searchers, and the number of previous failed searches.
 	 */
 	// Get the characters foraging skill bonus
-	const rm::rule::SubcategoriedSkillData& foraging_skill = config.object_factory.subcategoriedSkillData("SKILL_FORAGING");
+	const rm::rule::SubcategoriedSkillData& foraging_skill = modifiers.object_factory.subcategoriedSkillData("SKILL_FORAGING");
 
 	// Calculate the skill check roll plus additive modifiers for foraging
 	int foraging_roll = d100.roll().result();
 	foraging_roll += forager.skillBonus(foraging_skill);
-	foraging_roll += config.additional_searchers * 2; // Each additional searcher adds a +2 bonus to the roll
-	foraging_roll += config.failed_search_count * 5;  // Each previous failed search attempt adds a 5 bonus to the roll
-	LOG_DEBUG("Foraging skill check roll: {} (base roll: {}, skill bonus: {}, additional searchers bonus: {}, failed search bonus: {})", foraging_roll, d100.result(), forager.skillBonus(foraging_skill), config.additional_searchers * 2,
-	          config.failed_search_count * 5);
+	foraging_roll += modifiers.additional_searchers * 2; // Each additional searcher adds a +2 bonus to the roll
+	foraging_roll += modifiers.failed_search_count * 5;  // Each previous failed search attempt adds a 5 bonus to the roll
+	foraging_roll += modifiers.hunted ? -20 : 0;         // If the forager is being hunted, subtract 20 from the roll
+	LOG_DEBUG("Foraging skill check roll: {} (base roll: {}, skill bonus: {}, additional searchers bonus: {}, failed search bonus: {}, hunted penalty: {})", foraging_roll, d100.result(), forager.skillBonus(foraging_skill),
+	          modifiers.additional_searchers * 2, modifiers.failed_search_count * 5, modifiers.hunted ? -20 : 0);
 
 	// Calculate the multiplier based modifiers
-	float hours_modifier = static_cast<float>(config.hours_spent) / 10.0f; // Assuming 10 hours is the standard foraging time
+	float hours_modifier = static_cast<float>(modifiers.hours_spent) / 10.0f; // Assuming 10 hours is the standard foraging time
 
 	// Get the final forage roll by applying the hours modifier to the skill check roll
 	foraging_roll = static_cast<int>(foraging_roll * hours_modifier);
 
 	if (foraging_roll < 100) {
 		LOG_INFO("You failed to find any resources. Roll: {}", foraging_roll);
-		config.failed_search_count++;
+		modifiers.failed_search_count++;
 		return foraged_resources;
 	}
 
 	// Check if we are looking for a specific resource or a general forage
-	if (config.target_resource) {
-		const rm::rule::ForagableData& foragable = *config.target_resource;
+	if (modifiers.target_resource) {
+		const rm::rule::ForagableData& foragable = *modifiers.target_resource;
 
 		// Handle specific resource forage
 		if (foragable.location().matches(search_location)) {
@@ -57,7 +58,7 @@ std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::
 		}
 	} else {
 		// Handle general forage
-		for (const auto& foragable_wrapper : config.object_factory.getAll<rm::rule::ForagableData>()) {
+		for (const auto& foragable_wrapper : modifiers.object_factory.getAll<rm::rule::ForagableData>()) {
 			const rm::rule::ForagableData& foragable = foragable_wrapper.get();
 			if (foragable.location().matches(search_location)) {
 				int doses_roll = d100.roll().result();
@@ -77,9 +78,9 @@ std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::
 	std::map<const rm::rule::ForagableData*, int> identified_resources;
 
 	// Get the characters herb lore skill bonus
-	const rm::rule::SubcategoriedSkillData& herb_lore_skill = config.object_factory.subcategoriedSkillData("SKILL_HERB_LORE");
+	const rm::rule::SubcategoriedSkillData& herb_lore_skill = modifiers.object_factory.subcategoriedSkillData("SKILL_HERB_LORE");
 	int herb_lore_skill_bonus = forager.skillBonus(herb_lore_skill);
-	const rm::rule::SubcategoriedSkillData& poison_lore_skill = config.object_factory.subcategoriedSkillData("SKILL_POISON_LORE");
+	const rm::rule::SubcategoriedSkillData& poison_lore_skill = modifiers.object_factory.subcategoriedSkillData("SKILL_POISON_LORE");
 	int poison_lore_skill_bonus = forager.skillBonus(poison_lore_skill);
 
 	LOG_INFO("Foraged resources: Roll ({})", foraging_roll);
@@ -87,17 +88,16 @@ std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::
 	LOG_INFO("| {:<17} | {:^5} | {:^5} | {:^5} | {:^5} |", "Resource", "Diff", "Roll", "Doses", "Iden");
 	LOG_INFO("{:-<{}}", "", 52);
 
-
 	for (auto& [foragable, num_doses] : foraged_resources) {
 		int lore_roll = d100.roll().result();
 		if (foragable->loreSkill() == &herb_lore_skill) {
 			lore_roll += herb_lore_skill_bonus;
-			lore_roll += config.herb_lore_bonus; // Add any generic herb lore skill bonus from items (book)
+			lore_roll += modifiers.herb_lore_bonus; // Add any generic herb lore skill bonus from items (book)
 		} else if (foragable->loreSkill() == &poison_lore_skill) {
 			lore_roll += poison_lore_skill_bonus;
-			lore_roll += config.poison_lore_bonus; // Add any generic poison lore skill bonus from items (book)
+			lore_roll += modifiers.poison_lore_bonus; // Add any generic poison lore skill bonus from items (book)
 		}
-		lore_roll += config.lore_bonuses[foragable]; // Add any specific herb lore skill bonus from familiarity with specific plants
+		lore_roll += modifiers.lore_bonuses[foragable]; // Add any specific herb lore skill bonus from familiarity with specific plants
 		if (lore_roll < (foragable->difficultyRating() * 10)) {
 			LOG_INFO("| {:<17} | {:^5} | {:^5} | {:^5} | {:^5} |", foragable->name(), foragable->difficultyModifier(), lore_roll, num_doses, "No");
 		} else {
@@ -108,6 +108,219 @@ std::map<const rm::rule::ForagableData*, int> forage_herbs(rm::game::character::
 
 	LOG_INFO("{:-<{}}", "", 52);
 	return identified_resources;
+}
+
+float forage_food(rm::game::character::Character& forager, rm::game::Location& search_location, SustainanceModifiers& modifiers) {
+	// Get the characters foraging skill bonus
+	const rm::rule::SubcategoriedSkillData& foraging_skill = modifiers.object_factory.subcategoriedSkillData("SKILL_FORAGING");
+	float found_food = 0.0f;
+
+	using namespace rm::rule::enums::EnvironmentType;
+	using namespace rm::rule::enums::SkillDifficultyType;
+
+	// First we get the food difficulty
+	SkillDifficultyType::Type difficulty = kAbsurd;
+
+	// Deal with vegetation first
+	if (search_location.hasVegetation(Vegetation::kDeciduous) || search_location.hasVegetation(Vegetation::kConiferous) || search_location.hasVegetation(Vegetation::kJungle)) {
+		difficulty = std::min(difficulty, kEasy);
+	} else if (search_location.hasVegetation(Vegetation::kGrasslands) || search_location.hasVegetation(Vegetation::kHeath)) {
+		difficulty = std::min(difficulty, kMedium);
+	} else if (search_location.hasVegetation(Vegetation::kPlains)) {
+		difficulty = std::min(difficulty, kHard);
+	} else if (search_location.hasVegetation(Vegetation::kTundra)) {
+		difficulty = std::min(difficulty, kSheerFolly);
+	} else if (search_location.hasVegetation(Vegetation::kBarren)) {
+		difficulty = std::min(difficulty, kAbsurd);
+	}
+
+	// Now deal with water sources
+	if (search_location.hasWater(Water::kFreshCoast) || search_location.hasWater(Water::kLake) || search_location.hasWater(Water::kOasis)) {
+		difficulty = std::min(difficulty, kEasy);
+	} else if (search_location.hasWater(Water::kMarsh)) {
+		difficulty = std::min(difficulty, kMedium);
+	} else if (search_location.hasWater(Water::kSaltCoast)) {
+		difficulty = std::min(difficulty, kHard);
+	} else if (search_location.hasWater(Water::kDesert)) {
+		difficulty = std::min(difficulty, kSheerFolly);
+	} else if (search_location.hasWater(Water::kGlacier)) {
+		difficulty = std::min(difficulty, kAbsurd);
+	}
+
+	// If it is the dry season, and the water type is breaks or wadis, which are seasonal we set the minimum difficluty to kExtremlyHard
+	if (modifiers.dry_season && search_location.hasWater(Water::kBreaks)) {
+		difficulty = std::max(difficulty, kExtremelyHard); // Note this is a max because the dry season makes it very difficult to find water in breaks or wadis, even if there is vegetation present
+	}
+
+	// Alpine terrain is always diffuclt to forage in, so if it is present, we set the difficulty to very hard regardless of vegetation or water sources
+	if (search_location.hasTerrain(Terrain::kAlpine)) {
+		difficulty = std::max(difficulty, kVeryHard); // Note this is a max because alpine terrain is very difficult to forage in, even if there is vegetation or water present
+	}
+
+	// Apply the modifiers for the number of people searching, each additional person increases the difficulty by 1, capped at kAbsurd
+	difficulty = SkillDifficultyType::incrementDifficulty(difficulty, modifiers.num_people - 1); // Each additional person increases the difficulty by 1, capped at kAbsurd
+
+	// Apply the action duration modifiers
+	// If the duration is lower than 4 hours, the difficulty increases by 1 for each hour less than 4.
+	// If the duration is greater than 4 hours, the difficulty decreases by 1 for each 2 hours greater than 4, up to a maximum of 2 degrees of difficulty.
+	if (modifiers.hours_spent < 4) {
+		difficulty = SkillDifficultyType::incrementDifficulty(difficulty, 4 - modifiers.hours_spent); // Each hour less than 4 increases the difficulty by 1
+	} else if (modifiers.hours_spent > 4) {
+		int difficulty_reduction = (std::min(8, modifiers.hours_spent) - 4) / 2; // Each additional 2 hours spent foraging decreases the difficulty by 1 up to 8 hours reducing the dificulty by 2
+		difficulty = SkillDifficultyType::incrementDifficulty(difficulty, -difficulty_reduction);
+	}
+
+	// Make the skill check roll for foraging food
+	archid::Dice d100(100, 5); // Create a d100 dice
+	int foraging_roll = d100.roll().result();
+	foraging_roll += forager.skillBonus(foraging_skill);
+
+	if (foraging_roll == 66) {
+		LOG_INFO("You found a rare food source! Roll: {}", foraging_roll);
+		found_food = 1.1f; // Found a rare food source, which provides 10% more food than normal
+	} else if (foraging_roll == 100) {
+		LOG_INFO("You found a hidden food cache! Roll: {}", foraging_roll);
+		found_food = 1.25f; // Found a hidden food cache, which provides 25% more food than normal
+	} else if (foraging_roll <= 76) {
+		LOG_INFO("You return empty handed so no food today! Roll: {}", foraging_roll);
+		found_food = 0.0f; // Found no food
+	} else if (foraging_roll <= 90) {
+		LOG_INFO("You will be hungry today! Roll: {}", foraging_roll);
+		found_food = 0.2f; // Found 20% of the food needed for the day
+	} else if (foraging_roll <= 110) {
+		LOG_INFO("You get most of what you need! Roll: {}", foraging_roll);
+		found_food = 0.8f; // Found 80% of the food needed for the day
+	} else if (foraging_roll <= 175) {
+		LOG_INFO("Successful foraging food, you find what you need for the day! Roll: {}", foraging_roll);
+		found_food = 1.0f; // Found 100% of the food needed for the day
+	} else {
+		LOG_INFO("Outstanding success foraging food, you find more than you need for the day! Roll: {}", foraging_roll);
+		found_food = 1.2f; // Found 120% of the food needed for the day
+	}
+
+	return found_food;
+}
+
+float forage_water(rm::game::character::Character& forager, rm::game::Location& search_location, SustainanceModifiers& modifiers) {
+	// Get the characters foraging skill bonus
+	const rm::rule::SubcategoriedSkillData& foraging_skill = modifiers.object_factory.subcategoriedSkillData("SKILL_FORAGING");
+	float found_water = 0.0f;
+
+	using namespace rm::rule::enums::EnvironmentType;
+	using namespace rm::rule::enums::SkillDifficultyType;
+
+	// First we get the water difficulty
+	SkillDifficultyType::Type difficulty = kAbsurd;
+
+	// Deal with climate first
+	std::set<const rm::rule::ClimateData*> wet_climates{};
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_COLD_WET"));
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_COOL_WET"));
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_WARM_WET"));
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_COLD_TEMPERATE"));
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_COOL_TEMPERATE"));
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_WARM_TEMPERATE"));
+	wet_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_HOT_HUMID"));
+
+	for (const auto climate : wet_climates) {
+		if (search_location.hasClimate(climate)) {
+			difficulty = std::min(difficulty, kEasy);
+			break;
+		}
+	}
+
+	std::set<const rm::rule::ClimateData*> dry_climates{};
+	dry_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_COLD_DRY"));
+	dry_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_COOL_DRY"));
+	dry_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_WARM_DRY"));
+
+	for (const auto climate : dry_climates) {
+		if (search_location.hasClimate(climate)) {
+			difficulty = std::min(difficulty, kHard);
+			break;
+		}
+	}
+
+	std::set<const rm::rule::ClimateData*> arid_climates{};
+	arid_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_FRIGID"));
+	arid_climates.insert(&modifiers.object_factory.get<rm::rule::ClimateData>("CLIMATE_HOT_DRY"));
+
+	for (const auto climate : arid_climates) {
+		if (search_location.hasClimate(climate)) {
+			difficulty = std::min(difficulty, kExtremelyHard);
+			break;
+		}
+	}
+
+	// Now deal with water sources
+	if (search_location.hasWater(Water::kFreshCoast) || search_location.hasWater(Water::kLake) || search_location.hasWater(Water::kOasis)) {
+		difficulty = std::min(difficulty, kEasy);
+	} else if (search_location.hasWater(Water::kMarsh)) {
+		difficulty = std::min(difficulty, kMedium);
+	} else if (search_location.hasWater(Water::kDesert)) {
+		difficulty = std::min(difficulty, kSheerFolly);
+	}
+
+	// If it is the dry season, and the water type is breaks or wadis, which are seasonal we set the minimum difficluty to kExtremlyHard
+	if (modifiers.dry_season && search_location.hasWater(Water::kBreaks)) {
+		difficulty = std::max(difficulty, kExtremelyHard); // Note this is a max because the dry season makes it very difficult to find water in breaks or wadis, even if there is vegetation present
+	}
+
+	// Alpine terrain is always diffuclt to forage in, so if it is present, we set the difficulty to very hard regardless of vegetation or water sources
+	if (search_location.hasTerrain(Terrain::kAlpine)) {
+		difficulty = std::max(difficulty, kVeryHard); // Note this is a max because alpine terrain is very difficult to forage in, even if there is vegetation or water present
+	}
+
+	// Apply the action duration modifiers
+	// If the duration is lower than 4 hours, the difficulty increases by 1 for each hour less than 4.
+	// If the duration is greater than 4 hours, the difficulty decreases by 1 for each 2 hours greater than 4, up to a maximum of 2 degrees of difficulty.
+	if (modifiers.hours_spent < 4) {
+		difficulty = SkillDifficultyType::incrementDifficulty(difficulty, 4 - modifiers.hours_spent); // Each hour less than 4 increases the difficulty by 1
+	} else if (modifiers.hours_spent > 4) {
+		int difficulty_reduction = (std::min(8, modifiers.hours_spent) - 4) / 2; // Each additional 2 hours spent foraging decreases the difficulty by 1 up to 8 hours reducing the dificulty by 2
+		difficulty = SkillDifficultyType::incrementDifficulty(difficulty, -difficulty_reduction);
+	}
+
+	// Make the skill check roll for foraging water
+	archid::Dice d100(100, 5); // Create a d100 dice
+	int foraging_roll = d100.roll().result();
+	foraging_roll += forager.skillBonus(foraging_skill);
+
+	if (foraging_roll == 66) {
+		LOG_INFO("You found a rare water source! Roll: {}", foraging_roll);
+		found_water = 1.1f; // Found a rare water source, which provides 10% more water than normal
+	} else if (foraging_roll == 100) {
+		LOG_INFO("You found a hidden water cache! Roll: {}", foraging_roll);
+		found_water = 1.25f; // Found a hidden water cache, which provides 25% more water than normal
+	} else if (foraging_roll <= 76) {
+		LOG_INFO("You return empty handed searching for water! Roll: {}", foraging_roll);
+		found_water = 0.0f; // Found no water
+	} else if (foraging_roll <= 90) {
+		LOG_INFO("You will be thirsty today! Roll: {}", foraging_roll);
+		found_water = 0.2f; // Found 20% of the water needed for the day
+	} else if (foraging_roll <= 110) {
+		LOG_INFO("You get most of the water you need! Roll: {}", foraging_roll);
+		found_water = 0.8f; // Found 80% of the water needed for the day
+	} else if (foraging_roll <= 175) {
+		LOG_INFO("Successful foraging water, you find what you need for the day! Roll: {}", foraging_roll);
+		found_water = 1.0f; // Found 100% of the water needed for the day
+	} else {
+		LOG_INFO("Outstanding success foraging water, you find more than you need for the day! Roll: {}", foraging_roll);
+		found_water = 1.2f; // Found 120% of the water needed for the day
+	}
+
+	return found_water;
+}
+
+std::pair<float, float> forage_sustainance(rm::game::character::Character& forager, rm::game::Location& search_location, SustainanceModifiers& modifiers) {
+	// Get the characters foraging skill bonus
+	const rm::rule::SubcategoriedSkillData& foraging_skill = modifiers.object_factory.subcategoriedSkillData("SKILL_FORAGING");
+
+	// Make the food roll
+	float found_food = forage_food(forager, search_location, modifiers);
+	float found_water = forage_water(forager, search_location, modifiers);
+
+	return {found_food, found_water};
 }
 
 } // namespace rm::action::forage
