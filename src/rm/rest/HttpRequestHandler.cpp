c552d9c581c41aafe2185b7efcd0828fe1e14a4d
Archid <0archid0@gmail.com>
2026-06-22 15:59:46 +0100

    Add REST endpoint to reload data
    Added a new REST endpoint to allow the source data to be reloaded from the JSON files, populatiung the cache.

--- src/rm/rest/HttpRequestHandler.cpp
+++ src/rm/rest/HttpRequestHandler.cpp
@@ -39,6 +39,17 @@ void HttpRequestHandler::handleRequest(const http::request<http::string_body>& r
 			response.set(http::field::content_type, "application/json");
 			response.body() = R"({"error": "Save Failed", "message": ")" + std::string(e.what()) + R"("})";
 		}
+	} else if (request.method() == http::verb::get && path.matchExact("/rmce/load")) {
+		try {
+			serial_manager_.load();
+			response.result(http::status::ok);
+			response.set(http::field::content_type, "application/json");
+			response.body() = R"({"load": "successful"})";
+		} catch (const std::exception& e) {
+			response.result(http::status::internal_server_error);
+			response.set(http::field::content_type, "application/json");
+			response.body() = R"({"error": "Load Failed", "message": ")" + std::string(e.what()) + R"("})";
+		}
 	} else if (path.match("/rmce/data/")) {
 		DataRequestHandler handler {serial_manager_};
 		handler.handleRequest(request, response);
